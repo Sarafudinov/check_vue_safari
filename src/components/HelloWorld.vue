bc670acd0ea817260d1f512faef499642b4f3c1a
Nicolae <kolia.sh12345@gmail.com>
2025-01-30 18:09:57 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <swiper 
+          :initial-slide="1"
+          :slides-per-view="1.15"
+          :space-between="10"
+          :centeredSlides="true"
+          :loop="false"
+          :slide-to-clicked-slide="true"
+          :touch-ratio="1.5"
+          :pagination="true"
+          :modules="modules" 
+          class="mySwiper">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
+</template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/pagination';
+
+  import './style.css';
+
+  // import required modules
+  import { Pagination } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Pagination],
+      };
+    },
+  };
+</script>
